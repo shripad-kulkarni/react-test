6080bf32e4e145cafe359c10ed92f9c87bfcdd84
shripad-kulkarni <shripadkulkarni6655@gmail.com>
2026-06-22 08:38:49 +0530

    tailwind imported

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import './App.css'
 function App() {
  
   return (
-    <>
-      <h1>Learning React</h1>
-      <>I am a Shripad</>
-    </>
+    <div className="bg-gray-800  text-white grid place-content-center pt-20 h-screen">
+      <h1  className="text-5xl p-4 border" >Learning React</h1>
+      <h3 className="text-xl p-10 text-red-500">I am a Shripad</h3>
+      <h2 className="text-blue-500 border p-4 rounded-xl">Tailwind CSS Imported Successfully</h2>
+    </div>
   )
 }
 
